82b5c894ac92a4491d31a6e05b4003136fea9e25
Saeed <s.safikhani@outlook.com>
2022-04-04 08:15:08 +0200

    finish day 25

--- day_25/main.py
+++ day_25/main.py
@@ -11,14 +11,28 @@ turtle.shape(background)
 data = pandas.read_csv("50_states.csv")
 number_of_states = len(data["state"])
 correct_answer = 0
+used_states = []
+state_to_learn = []
+all_states = data["state"].to_list()
 
 while correct_answer < number_of_states:
     input = screen.textinput(title=f"{correct_answer}/{number_of_states} Guess the state:",
                              prompt="What's another state name?").capitalize()
 
+    if input == "Exit":
+
+        for state in all_states:
+            if not state in used_states:
+                state_to_learn.append(state)
+        df_learn_state = pandas.DataFrame(state_to_learn)
+        df_learn_state.to_csv("states_to_learn.csv")
+        break
     if not len(data[data["state"] == input]) == 0:
-        correct_answer +=1
+        correct_answer += 1
         state_name = turtle.Turtle()
         state_name.penup()
-        state_name.setpos(data[data["state"] == input].x[0], data[data["state"] == input].y[0])
+        state_data = data[data["state"] == input]
+        state_name.setpos(int(state_data.x), int(state_data.y))
         state_name.write(input)
+        state_name.hideturtle()
+        used_states.append(input)
